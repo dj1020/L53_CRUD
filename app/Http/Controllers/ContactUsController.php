cddd9b8dbc3445d3f1b11d988c4bea6f84f3743e
Ken Lin <kenlin@kkbox.com>
2016-09-07 16:44:27 +0800

    建 ContactUsController 和 index method

---
+++ app/Http/Controllers/ContactUsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ContactUsController extends Controller
+{
+    public function index()
+    {
+        return 'This is Contact Us Page';
+    }
+}
