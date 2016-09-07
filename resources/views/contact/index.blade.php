de91a883f4a8afa9671d688e553c81c26fe17c89
Ken Lin <kenlin@kkbox.com>
2016-09-07 16:58:01 +0800

    建立 HTML 基本框架，引入 jQuery, Bootstrap

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,1 +1,20 @@
-This is conatct us view file.
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Document</title>
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+
+    <script src="/js/jquery-1.12.0.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
