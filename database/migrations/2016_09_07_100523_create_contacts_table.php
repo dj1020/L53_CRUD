184e4c4ede8dfcc97374056cce92fba0113d00b7
Ken Lin <kenlin@kkbox.com>
2016-09-07 18:17:46 +0800

    建立 Contact Model 和 migration 檔, php artisan make:model Contact -m

---
+++ database/migrations/2016_09_07_100523_create_contacts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email');
+            $table->string('phone')->nullable();
+            $table->string('message');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
