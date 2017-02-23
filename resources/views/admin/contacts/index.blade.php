a6eb3fe6bb26cc7a3047a64974ca1e19179996b1
Ken Lin <kenlin@kkbox.com>
2017-02-23 17:32:32 +0800

    建立 contacts 列表 view，套用 bootstrap

--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -1,2 +1,33 @@
-<h1>顯示 Contacts 列表</h1>
+@extends('layouts.app')
 
+@section('content')
+    <div class="container">
+        <h2>顯示 Contacts 列表</h2>
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Contact Content</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+            </tr>
+            <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
+@endsection
