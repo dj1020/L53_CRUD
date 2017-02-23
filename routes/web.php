e82fbe4c0c79807c12ba5f3aeae6b5362241f9c2
Ken Lin <kenlin@kkbox.com>
2017-02-23 17:12:34 +0800

    利用 make:auth 建立會員註冊、登入機制 php artisan make:auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 
 Route::get('/contactUs', 'ContactUsController@index');
 Route::post('/submitContact', 'ContactUsController@store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
